d082b8d98797c5bba7573d80d73ad927c55714da
rafael-rm <rafael.rmo@outlook.com>
2023-04-25 21:21:57 -0300

    Adicionado milissegundos no horário das logs.

--- src/functions/logs.py
+++ src/functions/logs.py
@@ -3,7 +3,7 @@ import logging
 
 def main():
     logging.basicConfig(level=logging.INFO, filename='logs.log', format='%(asctime)s [%(levelname)s] %(name)s (%(filename)s %(lineno)d) - %(message)s', datefmt='%d/%m/%Y %H:%M:%S', encoding='utf-8')
-    loggin_format = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s (%(filename)s %(lineno)d) - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
+    loggin_format = logging.Formatter('%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s (%(filename)s %(lineno)d) - %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
     console = logging.StreamHandler()
     console.setFormatter(loggin_format)
     logging.getLogger().addHandler(console)
